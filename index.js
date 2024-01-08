d21e890eecbc8f2e656bf788d45c40e2a579e387
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-08 12:09:38 -0300

    Iniciando JS.

---
+++ index.js
@@ -0,0 +1,4 @@
+const displayValorAnterior = document.getElementById('valor-anterior');
+const displayValorActual = document.getElementById('valor-actual');
+const botonesNumeros = document.querySelectorAll('.numero');
+const botonesOperadores = document.querySelectorAll('.operador');
