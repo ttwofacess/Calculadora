3227bb702b728b6fb7f337ec33864c9467c9ed3e
ttwofacess <20799318+ttwofacess@users.noreply.github.com>
2024-01-10 17:57:41 -0300

    Constructor Display.

---
+++ Display.js
@@ -0,0 +1,9 @@
+class Display {
+    constructor(displayValorAnterior, displayValorActual) {
+        this.displayValorActual = displayValorActual;
+        this.displayValorAnterior = displayValorAnterior;
+        this.calculador = new Calculadora();
+        this.valorActual = '';
+        this.valorAnterior = '';
+    }
+}
